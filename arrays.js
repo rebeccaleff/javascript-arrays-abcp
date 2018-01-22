0377cf7710cb932779341bbe407daff7140f28bb
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-22 19:03:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
   return array.unshift(element);
-  console.log(array);
 }
