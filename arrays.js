02e67853b83da2d0cd269d077cfe4ad8213e9624
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-22 19:04:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+function addElementToBeginningOfArray([1], [foo]){
+  return [1].unshift(foo);
 }
+
+function 
